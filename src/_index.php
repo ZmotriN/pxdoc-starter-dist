83338915d30565aa92a14dfa51f83c5922e00ca9
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-17 20:03:16 -0400

    First commit

---
+++ src/_index.php
@@ -0,0 +1,39 @@
+<?php 
+/**
+ * @type     article
+ * @title    PXDoc
+ * @icon     pxdoc/images/logo.svg
+ * @abstract PHP driven static website engine.
+ */
+?>
+
+<grostitre>Introduction</grostitre>
+
+
+<p><strong>PXDoc</strong> est une solution innovante qui permet de transformer facilement des fichiers PHP en pages statiques, optimisées pour être hébergées sur des plateformes comme GitHub et Jekyll. Grâce à l'intégration transparente de composants Vue.js, PXDoc facilite la création et la gestion de contenu interactif pour vos besoins spécifiques.</p>
+
+<h3>Caractéristiques Principales</h3>
+
+<ul>
+    <li><strong>Conversion Facile</strong> : Générez des sites statiques à partir de fichiers <em>PHP</em> sans effort.</li>
+    <li><strong>Compatible GitHub et Jekyll</strong> : Parfaitement adapté pour un déploiement rapide sur ces plateformes populaires.</li>
+    <li><strong>Composants Vue.js</strong> : Utilisez des composants modulaires et réutilisables pour une intégration simplifiée.</li>
+    <li><strong>Idéal pour les Cours en Ligne</strong> : Créez et gérez des modules pédagogiques dynamiques avec une interface utilisateur engageante.</li>
+</ul>
+
+
+
+
+
+<p>PXDoc est l'outil par excellence pour les développeurs et éducateurs souhaitant créer du contenu de qualité, rapidement et efficacement. Explorez les possibilités offertes par PXDoc pour transformer votre vision en réalité numérique.</p>
+
+
+<dots></dots>
+
+<grostitre>À propos</grostitre>
+
+<ul>
+    <li>Développeur: <em>Maxime Larrivée-Roy</em></li>
+</ul>
+
+<dots></dots>
